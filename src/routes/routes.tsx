fdcbd3616237bead667c380e21a0a917527148cf
ashraf56 <ashrafulfahim07@gmail.com>
2024-07-03 11:56:22 +0600

    router created

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,24 +2,40 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import About from "../page/About";
 import Contact from "../page/Contact";
+import AdminDashboard from "../page/admin/AdminDashboard";
+import CreateStudent from "../page/admin/CreateStudent";
 
 
 const router = createBrowserRouter([
 
-{
-    path:'/',
-    element:<App/>,
-    children:[
-        {
-            path:'about',
-            element:<About/>
-        },
-        {
-            path:'contact',
-            element:<Contact/>
-        },
-    ]
-}
+    {
+        path: '/',
+        element: <App />,
+        children: [
+            {
+                path: 'about',
+                element: <About />
+            },
+            {
+                path: 'contact',
+                element: <Contact />
+            },
+        ]
+    },
+    {
+        path: '/admin',
+        element: <App />,
+        children: [
+            {
+                index: true,
+                element: <AdminDashboard />
+            },
+            {
+                path: 'create-student',
+                element: <CreateStudent />
+            },
+        ]
+    }
 
 ])
 
