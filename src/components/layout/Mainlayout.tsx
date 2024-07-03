a91bc33bc340c0cc4f578e8d09cd310062d9a7af
ashraf56 <ashrafulfahim07@gmail.com>
2024-07-03 11:53:52 +0600

    Outlet added

--- src/components/layout/Mainlayout.tsx
+++ src/components/layout/Mainlayout.tsx
@@ -7,6 +7,7 @@ import {
     VideoCameraOutlined,
 } from '@ant-design/icons';
 import { Button, Layout, Menu, theme } from 'antd';
+import { Outlet } from 'react-router-dom';
 
 const { Header, Sider, Content } = Layout;
 
@@ -71,7 +72,7 @@ const Mainlayout: React.FC = () => {
                         borderRadius: borderRadiusLG,
                     }}
                 >
-                    Content
+                    <Outlet/>
                 </Content>
             </Layout>
         </Layout>
