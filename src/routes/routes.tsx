d59b2242ad975b71d10024071eb22c3fe4ce803e
ashraf56 <ashrafulfahim07@gmail.com>
2024-07-03 11:52:53 +0600

    router created

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,1 +1,28 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import About from "../page/About";
+import Contact from "../page/Contact";
 
+
+const router = createBrowserRouter([
+
+{
+    path:'/',
+    element:<App/>,
+    children:[
+        {
+            path:'about',
+            element:<About/>
+        },
+        {
+            path:'contact',
+            element:<Contact/>
+        },
+    ]
+}
+
+])
+
+
+
+export default router
