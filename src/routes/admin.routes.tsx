e391a7f78ba0b6280731099ffd4fd2f03a1f22f4
ashraf56 <ashrafulfahim07@gmail.com>
2024-07-06 11:52:30 +0600

    admin route created

--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -1,9 +1,10 @@
+import { ReactNode } from "react";
 import AdminDashboard from "../page/admin/AdminDashboard";
 import CreateStudent from "../page/admin/CreateStudent";
 
 
 
-export const adminroutes = [
+export const adminroutesss = [
     {
         path: 'dashboard',
         element: <AdminDashboard />
@@ -13,3 +14,54 @@ export const adminroutes = [
         element: <CreateStudent />
     },
 ]
+
+export const adminPaths = [
+    {
+        name: 'Dashboard',
+        path: 'dashboard',
+        element: <AdminDashboard />,
+    },
+    {
+        name: 'User Management',
+        children: [
+            {
+                name: 'Create Student',
+                path: 'create-student',
+                element: <CreateStudent />,
+            },
+            {
+                name: 'Create Member',
+                path: 'create-member',
+                element: <CreateStudent />,
+            },
+        ],
+    },
+];
+
+
+
+type TRoute = {
+    path: string;
+    element: ReactNode;
+};
+
+
+export const Adminroutes = adminPaths.reduce((acc: TRoute[], item) => {
+
+    if (item.path && item.element) {
+        acc.push({
+            path: item.path,
+            element: item.element
+        })
+    }
+    if (item.children) {
+        item.children.forEach((child) => {
+            acc.push({
+                path: child.path,
+                element: child.element,
+            });
+        });
+    }
+
+    return acc
+}, [])
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import About from "../page/About";
 import Contact from "../page/Contact";
-import { adminroutes } from "./admin.routes";
+import { Adminroutes } from "./admin.routes";
 
 
 const router = createBrowserRouter([
@@ -24,7 +24,7 @@ const router = createBrowserRouter([
     {
         path: '/admin',
         element: <App />,
-        children:adminroutes
+        children:Adminroutes
     }
 
 ])
