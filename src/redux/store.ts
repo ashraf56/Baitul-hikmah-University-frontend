78173d26999679cb3ca590231c85ca9b6f9560b5
ashraf56 <ashrafulfahim07@gmail.com>
2024-07-07 08:09:24 +0600

    auth reducer added in the store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authslice from "./features/auth/authslice";
 
 
 
 export const store = configureStore({
-    reducer: {}
+    reducer: {
+        auth:authslice
+    }
 })
 
 
