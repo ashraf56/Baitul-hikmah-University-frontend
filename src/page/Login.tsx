7dc309544fabbe3d1774a8267517b392267f2c52
ashraf56 <ashrafulfahim07@gmail.com>
2024-07-07 13:09:43 +0600

    login an user with token success

--- src/page/Login.tsx
+++ src/page/Login.tsx
@@ -5,42 +5,43 @@ import { useForm } from "react-hook-form";
 import { useLoginsMutation } from "../redux/features/auth/authApi";
 import { useAppDispatch } from "../redux/hook";
 import { setUser } from "../redux/features/auth/authslice";
+import { verifyToken } from "../utills/verifyToken";
 
 const Login = () => {
 
-const dispatch = useAppDispatch()
-const {register,handleSubmit}  = useForm()
-const [login,{data,error}] = useLoginsMutation()
+    const dispatch = useAppDispatch()
+    const { register, handleSubmit } = useForm()
+    const [login, { data, error }] = useLoginsMutation()
 
-console.log({data});
-console.log(error);
+    console.log({ data });
+    console.log(error);
 
 
 
-const onsubmit = async (data:any) =>{
-  
-    const userInfo ={
-        id:data.id,
-        password:data.password
+    const onsubmit = async (data: any) => {
+
+        const userInfo = {
+            id: data.id,
+            password: data.password
+        }
+        const res = await login(userInfo).unwrap()
+        const user = verifyToken(res.data.accessToken)
+        dispatch(setUser({ user: user, token: res.accessToken }))
+
     }
-    const res = await login(userInfo).unwrap()
-
-    dispatch(setUser({user:{},token:res.accessToken}))
-    
-}
     return (
-        <div style={{width:'50%', margin:'auto'}}>
-             <form onSubmit={handleSubmit(onsubmit)}>
-      <div>
-        <label htmlFor="id">ID: </label>
-        <input type="text" id="id" {...register('id')} />
-      </div>
-      <div>
-        <label htmlFor="password">Password: </label>
-        <input type="text" id="password" {...register('password')} />
-      </div>
-      <Button htmlType="submit">Login</Button>
-    </form>
+        <div style={{ width: '50%', margin: 'auto' }}>
+            <form onSubmit={handleSubmit(onsubmit)}>
+                <div>
+                    <label htmlFor="id">ID: </label>
+                    <input type="text" id="id" {...register('id')} />
+                </div>
+                <div>
+                    <label htmlFor="password">Password: </label>
+                    <input type="text" id="password" {...register('password')} />
+                </div>
+                <Button htmlType="submit">Login</Button>
+            </form>
         </div>
     );
 };
--- src/utills/jwtDecode.ts
+++
@@ -1,5 +0,0 @@
-import { jwtDecode } from "jwt-decode"
-
-export const VerifyToken = (token:string)=>{
-    jwtDecode(token)
-}
