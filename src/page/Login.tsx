69f263c9d4cf3bafd0a0f9d808ea01e5dfd396e3
ashraf56 <ashrafulfahim07@gmail.com>
2024-07-07 12:57:55 +0600

    initial log in completed

--- src/page/Login.tsx
+++ src/page/Login.tsx
@@ -1,62 +1,48 @@
-import React from 'react';
-import type { FormProps } from 'antd';
-import { Button, Checkbox, Form, Input } from 'antd';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Button } from "antd";
 
-type FieldType = {
-  username?: string;
-  password?: string;
-  remember?: string;
+import { useForm } from "react-hook-form";
+import { useLoginsMutation } from "../redux/features/auth/authApi";
+import { useAppDispatch } from "../redux/hook";
+import { setUser } from "../redux/features/auth/authslice";
+
+const Login = () => {
+
+const dispatch = useAppDispatch()
+const {register,handleSubmit}  = useForm()
+const [login,{data,error}] = useLoginsMutation()
+
+console.log({data});
+console.log(error);
+
+
+
+const onsubmit = async (data:any) =>{
+  
+    const userInfo ={
+        id:data.id,
+        password:data.password
+    }
+    const res = await login(userInfo).unwrap()
+
+    dispatch(setUser({user:{},token:res.accessToken}))
+    
+}
+    return (
+        <div style={{width:'50%', margin:'auto'}}>
+             <form onSubmit={handleSubmit(onsubmit)}>
+      <div>
+        <label htmlFor="id">ID: </label>
+        <input type="text" id="id" {...register('id')} />
+      </div>
+      <div>
+        <label htmlFor="password">Password: </label>
+        <input type="text" id="password" {...register('password')} />
+      </div>
+      <Button htmlType="submit">Login</Button>
+    </form>
+        </div>
+    );
 };
-
-const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
-  console.log('Success:', values);
-};
-
-const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
-  console.log('Failed:', errorInfo);
-};
-
-const Login: React.FC = () => (
-  <Form
-    name="basic"
-    labelCol={{ span: 8 }}
-    wrapperCol={{ span: 16 }}
-    style={{ maxWidth: 600 }}
-    initialValues={{ remember: true }}
-    onFinish={onFinish}
-    onFinishFailed={onFinishFailed}
-    autoComplete="off"
-  >
-    <Form.Item<FieldType>
-      label="Username"
-      name="username"
-      rules={[{ required: true, message: 'Please input your username!' }]}
-    >
-      <Input />
-    </Form.Item>
-
-    <Form.Item<FieldType>
-      label="Password"
-      name="password"
-      rules={[{ required: true, message: 'Please input your password!' }]}
-    >
-      <Input.Password />
-    </Form.Item>
-
-    <Form.Item<FieldType>
-      name="remember"
-      valuePropName="checked"
-      wrapperCol={{ offset: 8, span: 16 }}
-    >
-      <Checkbox>Remember me</Checkbox>
-    </Form.Item>
-
-    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-      <Button type="primary" htmlType="submit">
-        Submit
-      </Button>
-    </Form.Item>
-  </Form>
-);
 
 export default Login;
--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -3,6 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseapi = createApi({
     reducerPath: 'baseapi',
-    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api/v1" }),
+    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api/v1",credentials:'include' }),
+
     endpoints: () => ({}),
 })
