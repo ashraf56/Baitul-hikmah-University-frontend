08af5e4ebb4d8a68377d89cb022d781542ff3823
ashraf56 <ashrafulfahim07@gmail.com>
2024-07-08 10:51:38 +0600

    persister added in the store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,22 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { configureStore } from "@reduxjs/toolkit";
-import authslice from "./features/auth/authslice";
+import authReducer from "./features/auth/authslice";
 import { baseapi } from "./api/baseApi";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
 
 
+const configPersistor = {
+    key: 'auth',
+    storage
+}
+
+const persistedAuth = persistReducer(configPersistor, authReducer)
 
 export const store = configureStore({
     reducer: {
         [baseapi.reducerPath]: baseapi.reducer,
-        auth: authslice
+        auth: persistedAuth
     },
     middleware: (getDefaultMiddlewares) => getDefaultMiddlewares().concat(baseapi.middleware)
 })
@@ -18,3 +27,5 @@ export const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
+
+export const persistor = persistStore(store)
