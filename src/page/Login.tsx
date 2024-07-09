49036ce1484a3cee3d2be3a028f0d874055b01dc
ashraf56 <ashrafulfahim07@gmail.com>
2024-07-09 17:07:16 +0600

    log in redirect completed

--- src/page/Login.tsx
+++ src/page/Login.tsx
@@ -8,6 +8,7 @@ import { setUser } from "../redux/features/auth/authslice";
 import { verifyToken } from "../utills/verifyToken";
 import { TUser } from "../Types";
 import { toast } from "sonner";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
 
@@ -15,10 +16,12 @@ const Login = () => {
     const { register, handleSubmit } = useForm()
     const [login] = useLoginsMutation()
 
+ const navigate = useNavigate()
 
-  
     const onsubmit = async (data: any) => {
-  const toast1 = toast.loading('loading...')
+        const toast1 = toast.loading('loading...', {duration:2000})  
+        try {
+    
         const userInfo = {
             id: data.id,
             password: data.password
@@ -27,7 +30,12 @@ const Login = () => {
         const user = verifyToken(res.data.accessToken) as TUser
         dispatch(setUser({ user: user, token: res.data.accessToken }))
         toast.success('Logged in', { id: toast1, duration: 2000 });
-
+        navigate(`/${user.role}/dashboard`)
+      
+      } catch (error) {
+        toast.error('Faild to log in', {  duration: 2000 });
+      
+      }
 
     }
     return (
--- src/page/admin/AcademicManegment/Academic_semister.tsx
+++ src/page/admin/AcademicManegment/Academic_semister.tsx
@@ -1,5 +1,8 @@
+import { useGetsemisterQuery } from "../../../redux/features/academicSemister/academicsemisterApi";
 
 const Academic_semister = () => {
+    const {data} = useGetsemisterQuery(undefined)
+    console.log(data);
     
     return (
         <div>
