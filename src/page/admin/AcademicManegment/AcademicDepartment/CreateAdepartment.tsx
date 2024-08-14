df178f489a09a8073e51e1c6ea40edbc3ec420b7
ashraf56 <ashrafulfahim07@gmail.com>
2024-08-14 09:34:23 +0600

    academic department created from front end

--- src/page/admin/AcademicManegment/AcademicDepartment/CreateAdepartment.tsx
+++ src/page/admin/AcademicManegment/AcademicDepartment/CreateAdepartment.tsx
@@ -5,37 +5,49 @@ import CustomSelect from "../../../../components/form/CustomSelect";
 import { FieldValues, SubmitHandler } from "react-hook-form";
 import { useGetAcademicFacultyQuery } from "../../../../redux/features/academicFaculty/academicFaculty";
 import CustomInput from "../../../../components/form/CustomInput";
+import { useCreateAcademicDepartmentMutation } from "../../../../redux/features/academicDepartment/academicDepartmentApi";
+import { toast } from "sonner";
 
 const CreateAdepartment = () => {
+    const [createAcademicDepartment] = useCreateAcademicDepartmentMutation()
 
-    const {data:Afaculty} = useGetAcademicFacultyQuery(undefined)
+    const { data: Afaculty } = useGetAcademicFacultyQuery(undefined)
 
 
     const faculTyOptions = Afaculty?.data?.map((item: any) => ({
         value: item._id,
         label: item.name,
-      }));
+    }));
     const onSubmit: SubmitHandler<FieldValues> = async (data) => {
-        
-console.log(data);
 
-        
-       
+        const toastId = toast.loading('Creating...');
+
+        try {
+            const res = await createAcademicDepartment(data) as any
+            if (res.error) {
+                toast.error(res.error?.data?.message, { id: toastId })
+            } else {
+                toast.success("Department created", { id: toastId })
+            }
+        } catch (error) {
+            toast.error('something error', { id: toastId })
+        }
+
 
     }
     return (
         <div>
-             <Flex justify="center" align="center">
+            <Flex justify="center" align="center">
                 <Col span={8}>
                     <CustomForm
                         onSubmit={onSubmit}   >
 
-                         <CustomInput type="text" placeholder="Enter name" name="name" label="Department Name" />
+                        <CustomInput type="text" placeholder="Enter name" name="name" label="Department Name" />
 
                         <CustomSelect placeholder='Select Faculty' label="Academic Faculty" name="academicFaculty" options={faculTyOptions} />
-                        
-                        
-                        
+
+
+
                         <Button htmlType="submit">Submit</Button>
                     </CustomForm>
                 </Col>
